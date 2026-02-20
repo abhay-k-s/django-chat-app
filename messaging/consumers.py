e902774d04974bc0e6fc53ac283468c44fd86895
Abhay K S <abhayks561@gmail.com>
2026-02-20 22:30:33 +0530

    Corrected comsumer bugs

--- messaging/consumers.py
+++ messaging/consumers.py
@@ -15,11 +15,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         self.user = self.scope["user"]
         self.other_user_id = int(self.scope["url_route"]["kwargs"]["user_id"])
+
         user1 = self.user.id
         user2 = self.other_user_id
         self.room_name = f"chat_{min(user1, user2)}_{max(user1, user2)}"
-        await self.channel_layer.group_add(self.room_name,self.channel_name)
+
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+
         await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_name,self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -29,15 +38,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return
 
         receiver = await sync_to_async(User.objects.get)(id=self.other_user_id)
-        await sync_to_async(Message.objects.create)(sender=self.user,receiver=receiver,content=message)
+        msg = await sync_to_async(Message.objects.create)(sender=self.user,receiver=receiver,content=message)
+
         await self.channel_layer.group_send(
             self.room_name,
             {
                 "type": "chat_message",
+                "sender_id": self.user.id,
                 "sender": self.user.username,
-                "message": message
+                "message": message,
+                "message_id": msg.id,
             }
         )
 
     async def chat_message(self, event):
-        await self.send(text_data=json.dumps({"sender": event["sender"],"message": event["message"]}))
+        await self.send(text_data=json.dumps({"sender_id": event["sender_id"],"sender": event["sender"],"message": event["message"],"message_id": event["message_id"],}))
